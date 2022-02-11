4bb648d99be41b1b7147d66e215671542de754b0
Volodymyr Yanyshyn <yanyshynv@gmail.com>
2022-02-11 13:13:02 +0200

    add web-browser module

---
+++ Runtime/Modules/WebBrowser/Plugins/WebGL/web-browser.jslib
@@ -0,0 +1,160 @@
+var unityWebBrowser = {
+
+  // TAKED FROM IS.JS LIBRARY
+  // https://github.com/arasatasaygin/is.js/blob/master/is.js
+  $browser:{
+    userAgent: function(){
+      return (window.navigator && window.navigator.userAgent || '').toLowerCase();
+    },
+    platform: function(){
+      return (window.navigator && window.navigator.platform || '').toLowerCase();
+    },
+    vendor: function(){
+      return (window.navigator && window.navigator.vendor || '').toLowerCase();
+    },
+  },
+
+  userAgent: function(){
+    return browser.userAgent();
+  },
+  platform: function(){
+    return browser.platform();
+  },
+  vendor: function(){
+    return browser.vendor();
+  },
+
+  // BROWSERS 
+  isIE: function() {
+    return browser.userAgent().match(/(?:msie |trident.+?; rv:)(\d+)/) !== null;
+  },
+  isOpera: function() {
+    return browser.userAgent().match(/(?:^opera.+?version|opr)\/(\d+)/) !== null;
+  },
+  isOperaMini: function() {
+    return browser.userAgent().match(/opera mini\/(\d+)/) !== null;
+  },
+  isEdge: function() {
+    return browser.userAgent().match(/edge\/(\d+)/) !== null;
+  },
+  isChrome: function() {
+    var vendor = (window.navigator && window.navigator.vendor || '').toLowerCase();
+    return (/google inc/.test(vendor) ? browser.userAgent().match(/(?:chrome|crios)\/(\d+)/) : null) !== null;
+  },
+  isSafari: function() {
+    return browser.userAgent().match(/version\/(\d+).+?safari/) !== null;
+  },
+  isFirefox: function() {
+    return browser.userAgent().match(/(?:firefox|fxios)\/(\d+)/) !== null;
+  },
+
+  // DEVICE
+  isAndroid: function() {
+    return /android/.test(browser.userAgent());
+  },
+  isAndroidPhone: function() {
+    return /android/.test(browser.userAgent()) && /mobile/.test(browser.userAgent());
+  },
+  isAndroidTablet: function() {
+    return /android/.test(browser.userAgent()) && !/mobile/.test(browser.userAgent());
+  },
+
+  isiPad: function() {
+    return browser.userAgent().match(/ipad.+?os (\d+)/) !== null || (browser.userAgent().match(/macintosh/) !== null && "ontouchend" in document);
+  },
+  isiPod: function() {
+    return browser.userAgent().match(/ipod.+?os (\d+)/) !== null;
+  },
+  isiPhone: function(){
+    return browser.userAgent().match(/iphone(?:.+?os (\d+))?/) !== null;
+  },
+
+  isWindows: function() {
+    return /win/.test(browser.platform());
+  },
+  isWindowsPhone: function() {
+    return /phone/.test(browser.userAgent());
+  },
+  isWindowsTablet: function() {
+    return /touch/.test(browser.userAgent());
+  },
+  
+  isBlackberry: function() {
+    return /blackberry/.test(browser.userAgent()) || /bb10/.test(browser.userAgent());
+  },
+
+  sendBeacon: function(url, json){
+    url = Pointer_stringify(url);
+    json = Pointer_stringify(json);
+
+    console.log('[WEB] Send Beacon: url = ',url);
+    console.log('[WEB] Send Beacon: body = ',json);
+    window.navigator.sendBeacon(url, json);
+  },
+
+  postMessage: function(message){
+    var msg = Pointer_stringify(message);
+    console.log('[WEB] Post Message: ', msg);
+    window.parent.postMessage(msg, '*');
+  },
+  
+  setLocation: function(new_url){
+    var url = Pointer_stringify(new_url);
+    var iframe = false;
+    try {
+      iframe = (window.self !== window.top);
+    } catch (e) {
+      iframe = true;
+    }
+    if(iframe){
+      window.top.location.href = url;
+    }else{
+      window.location.href = url;
+    }
+  },
+
+  inIFrame: function(){
+    try {
+      return window.self !== window.top;
+    } catch (e) {
+      return true;
+    }
+  },
+
+  isFullScreen: function() {
+    var element = document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement || null;
+    if(element === null) return false;
+    else return true;
+  },
+
+  enterFullScreen: function(){
+    var element = document.body;
+    try{
+      if(element.requestFullscreen) element.requestFullscreen();
+      else if(element.mozRequestFullScreen) element.mozRequestFullScreen();
+      else if(element.webkitRequestFullscreen) element.webkitRequestFullscreen();
+      else if(element.msRequestFullscreen) element.msRequestFullscreen();
+    }catch(e){
+      console.log("[Error] enterFullScreen: ", e);
+    }
+    
+  },
+
+  exitFullScreen: function(){
+    try{
+      if(document.exitFullscreen) document.exitFullscreen();
+      else if(document.mozCancelFullScreen) document.mozCancelFullScreen();
+      else if(document.webkitExitFullscreen) document.webkitExitFullscreen();
+      else if(document.msExitFullscreen) document.msExitFullscreen();
+    }catch(e){
+      console.log("[Error] exitFullScreen: ", e);
+    }
+  },
+
+  reloadPage: function(){
+    window.location.reload();
+  }
+};
+
+autoAddDeps(unityWebBrowser, '$browser');
+mergeInto(LibraryManager.library, unityWebBrowser);
---
+++ Runtime/Modules/WebBrowser/WebBrowser.cs
@@ -0,0 +1,163 @@
+﻿#if UNITY_WEBGL
+using System.Runtime.InteropServices;
+#endif
+
+namespace KAU.FireballSDK.Modules
+{
+    public static class WebBrowser
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+
+        [DllImport("__Internal")] private static extern string userAgent();
+        [DllImport("__Internal")] private static extern string platform();
+        [DllImport("__Internal")] private static extern string vendor();
+
+        [DllImport("__Internal")] private static extern bool isiPhone();
+        [DllImport("__Internal")] private static extern bool isiPad();
+        [DllImport("__Internal")] private static extern bool isiPod();
+
+        [DllImport("__Internal")] private static extern bool isAndroid();
+        [DllImport("__Internal")] private static extern bool isAndroidPhone();
+        [DllImport("__Internal")] private static extern bool isAndroidTablet();
+
+        [DllImport("__Internal")] private static extern bool isWindows();
+        [DllImport("__Internal")] private static extern bool isWindowsPhone();
+        [DllImport("__Internal")] private static extern bool isWindowsTablet();
+
+        [DllImport("__Internal")] private static extern bool isBlackberry();
+
+        [DllImport("__Internal")] private static extern bool isIE();
+        [DllImport("__Internal")] private static extern bool isEdge();
+        [DllImport("__Internal")] private static extern bool isChrome();
+        [DllImport("__Internal")] private static extern bool isSafari();
+        [DllImport("__Internal")] private static extern bool isFirefox();
+        [DllImport("__Internal")] private static extern bool isOpera();
+        [DllImport("__Internal")] private static extern bool isOperaMini();
+
+        [DllImport("__Internal")] private static extern void sendBeacon(string url, string json);
+        [DllImport("__Internal")] private static extern void postMessage(string msg);
+        [DllImport("__Internal")] private static extern void setLocation(string url);
+
+        [DllImport("__Internal")] private static extern bool inIFrame();
+        [DllImport("__Internal")] private static extern bool isFullScreen();
+        [DllImport("__Internal")] private static extern void enterFullScreen();
+        [DllImport("__Internal")] private static extern void exitFullScreen();
+
+        [DllImport("__Internal")] private static extern void reloadPage();
+
+        public static string UserAgent => userAgent();
+        public static string Platform => platform();
+        public static string Vendor => vendor();
+
+        public static bool IsDesktop => !IsMobile && !IsTablet;
+        public static bool IsMobile => IsiPhone || IsiPod || IsAndroidPhone || IsBlackberry || IsWindowsPhone;
+        public static bool IsTablet => IsiPad || IsAndroidTablet || IsWindowsTablet;
+
+        public static bool IsiOS => IsiPad || IsiPod || IsiPhone;
+        public static bool IsiPhone => isiPhone() && !IsiPad;
+        public static bool IsiPad => isiPad();
+        public static bool IsiPod => isiPod();
+
+        public static bool IsAndroid => isAndroid();
+        public static bool IsAndroidPhone => isAndroidPhone();
+        public static bool IsAndroidTablet => isAndroidTablet();
+
+        public static bool IsWindows => isWindows();
+        public static bool IsWindowsPhone => IsWindows && isWindowsPhone();
+        public static bool IsWindowsTablet => IsWindows && !IsWindowsPhone && isWindowsTablet();
+
+        public static bool IsBlackberry => isBlackberry();
+
+        public static bool IsIE => isIE();
+        public static bool IsEdge => isEdge();
+        public static bool IsChrome => isChrome() && !IsOpera;
+        public static bool IsSafari => isSafari();
+        public static bool IsFirefox => isFirefox();
+        public static bool IsOpera => isOpera();
+        public static bool IsOperaMini => isOperaMini();
+
+
+        public static void SendBeacon(string url, string json)
+        {
+            sendBeacon(url, json);
+        }
+
+        public static void PostMessage(string msg)
+        {
+            postMessage(msg);
+        }
+
+        public static void SetLocation(string url)
+        {
+            setLocation(url);
+        }
+
+        public static bool InIFrame => inIFrame();
+        public static bool IsFullScreen => isFullScreen();
+        public static void ToggleFullScreen()
+        {
+            UnityEngine.Debug.Log($"[WINDOW] IsFullScreen = {IsFullScreen}");
+            if (IsFullScreen) ExitFullScreen();
+            else EnterFullScreen();
+        }
+        public static void EnterFullScreen()
+        {
+            enterFullScreen();
+        }
+        public static void ExitFullScreen()
+        {
+            exitFullScreen();
+        }
+        public static void ReloadPage()
+        {
+            reloadPage();
+        }
+
+#else
+
+        public static string UserAgent => "";
+        public static string Platform => "";
+        public static string Vendor => "";
+
+        public static bool IsDesktop => false;
+        public static bool IsMobile => false;
+        public static bool IsTablet => false;
+
+        public static bool IsiOS => false;
+        public static bool IsiPhone => false;
+        public static bool IsiPad => false;
+        public static bool IsiPod => false;
+
+        public static bool IsAndroid => false;
+        public static bool IsAndroidPhone => false;
+        public static bool IsAndroidTablet => false;
+
+        public static bool IsWindows => false;
+        public static bool IsWindowsPhone => false;
+        public static bool IsWindowsTablet => false;
+
+        public static bool IsBlackberry => false;
+
+        public static bool IsIE => false;
+        public static bool IsEdge => false;
+        public static bool IsChrome => false;
+        public static bool IsSafari => false;
+        public static bool IsFirefox => false;
+        public static bool IsOpera => false;
+        public static bool IsOperaMini => false;
+
+        public static void SendBeacon(string url, string json) { }
+        public static void PostMessage(string msg) { }
+        public static void SetLocation(string url) { }
+
+        public static bool InIFrame => false;
+        public static bool IsFullScreen => false;
+        public static void ToggleFullScreen() { }
+        public static void EnterFullScreen() { }
+        public static void ExitFullScreen() { }
+
+        public static void ReloadPage() { }
+
+#endif
+    }
+}
