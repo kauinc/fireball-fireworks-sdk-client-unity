ebbced8e7aa77c325d0286c2d5fe9590fc19ae4d
Volodymyr <yanyshynv@gmail.com>
2023-08-11 11:02:19 +0300

    added client script error button on dialog

--- Runtime/Models/Error/ErrorDialog.cs
+++ Runtime/Models/Error/ErrorDialog.cs
@@ -8,6 +8,7 @@ namespace Fireball.Game.Client.Models
         Reload = 1,
         Retry = 2,
         Redirect = 3,
+        ClientScript = 4,
     }
 
     public class ErrorDialog
@@ -29,7 +30,7 @@ namespace Fireball.Game.Client.Models
         {
             return new ErrorDialog(title, message, new List<ErrorDialogButton>()
             {
-                new ErrorDialogButton(buttonLabel, DialogButtonAction.OK)
+                 ErrorDialogButton.ButtonOk(buttonLabel)
             });
         }
     }
@@ -39,21 +40,55 @@ namespace Fireball.Game.Client.Models
         public string Text { get; set; }
         public DialogButtonAction Action { get; set; }
         public string Link { get; set; } // Require for Action type Redirect/3
+        public ErrorClientScript ClientScript { get; set; } //Required for Action type ClientScript/4
 
         public ErrorDialogButton() { }
 
-        public ErrorDialogButton(string text, DialogButtonAction action)
+        public static ErrorDialogButton ButtonOk(string text = "OK")
         {
-            Text = text;
-            Action = action;
-            Link = null;
+            return new ErrorDialogButton()
+            {
+                Text = text,
+                Action = DialogButtonAction.OK,
+            };
         }
 
-        public ErrorDialogButton(string text, string link)
+        public static ErrorDialogButton ButtonReload(string text = "Reload")
         {
-            Text = text;
-            Action = DialogButtonAction.Redirect;
-            Link = link;
+            return new ErrorDialogButton()
+            {
+                Text = text,
+                Action = DialogButtonAction.Reload,
+            };
+        }
+
+        public static ErrorDialogButton ButtonRetry(string text = "Retry")
+        {
+            return new ErrorDialogButton()
+            {
+                Text = text,
+                Action = DialogButtonAction.Retry,
+            };
+        }
+
+        public static ErrorDialogButton ButtonRedirect(string text, string link)
+        {
+            return new ErrorDialogButton()
+            {
+                Text = text,
+                Action = DialogButtonAction.Redirect,
+                Link = link,
+            };
+        }
+
+        public static ErrorDialogButton ButtonClientScript(string text, ErrorClientScript script)
+        {
+            return new ErrorDialogButton()
+            {
+                Text = text,
+                Action = DialogButtonAction.ClientScript,
+                ClientScript = script,
+            };
         }
     }
 }
