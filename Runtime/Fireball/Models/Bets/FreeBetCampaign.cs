8887ce53efd46fc169bcf16a981a85cfddd300c3
Volodymyr <yanyshynv@gmail.com>
2024-01-18 10:43:48 +0200

    fbc json property

--- Runtime/Fireball/Models/Bets/FreeBetCampaign.cs
+++ Runtime/Fireball/Models/Bets/FreeBetCampaign.cs
@@ -5,11 +5,19 @@ using Newtonsoft.Json;
 namespace Fireball.Game.Client.Models
 {
     [UnityEngine.Scripting.Preserve]
+    [JsonObject(MemberSerialization.OptIn)]
     public class FreeBetCampaign
     {
+        [JsonProperty]
         public string Id { get; set; }
+
+        [JsonProperty]
         public long BetAmount { get; set; }
+
+        [JsonProperty]
         public int NumberOfBets { get; set; }
+
+        [JsonProperty]
         public Dictionary<string, object> Settings { get; set; }
     }
 }
