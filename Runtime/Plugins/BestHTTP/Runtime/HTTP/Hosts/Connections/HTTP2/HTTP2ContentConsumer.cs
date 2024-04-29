585788071e71c68c28ae8dd8824189cfe9c2b03b
Volodymyr <yanyshynv@gmail.com>
2024-04-29 15:20:49 +0300

    fixed read only span inaccessable level for older unity

--- Runtime/Plugins/BestHTTP/Runtime/HTTP/Hosts/Connections/HTTP2/HTTP2ContentConsumer.cs
+++ Runtime/Plugins/BestHTTP/Runtime/HTTP/Hosts/Connections/HTTP2/HTTP2ContentConsumer.cs
@@ -141,8 +141,14 @@ namespace Best.HTTP.Hosts.Connections.HTTP2
                 {
                     // The client connection preface starts with a sequence of 24 octets
                     // Connection preface starts with the string PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n).
+#if UNITY_2021_1_OR_NEWER
                     ReadOnlySpan<byte> MAGIC = stackalloc byte[24] { 0x50, 0x52, 0x49, 0x20, 0x2a, 0x20, 0x48, 0x54, 0x54, 0x50, 0x2f, 0x32, 0x2e, 0x30, 0x0d, 0x0a, 0x0d, 0x0a, 0x53, 0x4d, 0x0d, 0x0a, 0x0d, 0x0a };
                     bufferedStream.Write(MAGIC);
+#else
+                    var MAGIC = new byte[24] { 0x50, 0x52, 0x49, 0x20, 0x2a, 0x20, 0x48, 0x54, 0x54, 0x50, 0x2f, 0x32, 0x2e, 0x30, 0x0d, 0x0a, 0x0d, 0x0a, 0x53, 0x4d, 0x0d, 0x0a, 0x0d, 0x0a };
+                    bufferedStream.Write(MAGIC, 0, 24);
+#endif
+
 
                     // This sequence MUST be followed by a SETTINGS frame (Section 6.5), which MAY be empty.
                     // The client sends the client connection preface immediately upon receipt of a
