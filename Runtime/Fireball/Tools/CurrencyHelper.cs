4cd34cbc178d39dbac0fbe32f66d4955c3eaac90
Volodymyr <yanyshynv@gmail.com>
2025-08-05 18:19:50 +0300

    added crypto currency

--- Runtime/Fireball/Tools/CurrencyHelper.cs
+++ Runtime/Fireball/Tools/CurrencyHelper.cs
@@ -15,7 +15,7 @@ namespace Fireball.Game.Client.Tools
             public readonly double DecimalsMultiplier;
             public readonly string Desc;
 
-            public CurrencyData(string code, string symbolFormat, int decimals = 2, string desc = null)
+            public CurrencyData(string code, string symbolFormat, int decimals = 2, double multiplier = 0.01d, string desc = null)
             {
                 Code = code;
                 SymbolFormat = symbolFormat;
@@ -31,24 +31,30 @@ namespace Fireball.Game.Client.Tools
         private static List<string> _currencies = new List<string>();
         private static readonly Dictionary<string, CurrencyData> _virtualCurrencies = new Dictionary<string, CurrencyData>
         {
-            { "GHC", new CurrencyData("GHC", "SC {0}", 2, "Markor Sweepstakes Coin") },
-            { "WOC", new CurrencyData("WOC", "SC {0}", 2, "Markor WOC Sweepstakes Coin") },
-            { "SSC", new CurrencyData("SSC", "SC {0}", 2, "Markor Sweepstakes Coin") },
-            { "SS1", new CurrencyData("SS1", "SC {0}", 2, "Hub88 Sweepstakes Coin") },
-            { "SC.", new CurrencyData("SC.", "SC {0}", 2, "Sweepstakes Coin via Relax P2P") },
-            { "XSC", new CurrencyData("XSC", "SC {0}", 2, "Generic Sweepstakes Coin") },
-            { "GCC", new CurrencyData("GCC", "GC {0}", 2, "Markor Gold Coin") },
-            { "XGC", new CurrencyData("XGC", "GC {0}", 2, "Generic Gold Coin") },
-            { "GC.", new CurrencyData("GC.", "GC {0}", 2, "Gold Coin via Relax P2P") },
-            { "GLD", new CurrencyData("GLD", "GC {0}", 2, "Hub88 Gold Coin") },
-            { "SFC", new CurrencyData("SFC", "FC {0}", 2, "Markor Sweepstakes Coin (Casimba)") },
-            { "FCC", new CurrencyData("FCC", "FC {0}", 2, "(Fortune) Sweepstakes Coin") },
-            { "FC.", new CurrencyData("FC.", "FC {0}", 2, "Relax Fortune Coin") },
-            { "BT.", new CurrencyData("BT.", "BT {0}", 2, "Relax Bit") },
-            { "BK.", new CurrencyData("BK.", "BK {0}", 2, "Relax Buck") },
-            { "VBC", new CurrencyData("VBC", "WOW {0}", 2, "Markor VBC Fun Coin") },
+            { "GHC", new CurrencyData("GHC", "SC {0}", desc: "Markor Sweepstakes Coin") },
+            { "WOC", new CurrencyData("WOC", "SC {0}", desc: "Markor WOC Sweepstakes Coin") },
+            { "SSC", new CurrencyData("SSC", "SC {0}", desc: "Markor Sweepstakes Coin") },
+            { "SS1", new CurrencyData("SS1", "SC {0}", desc: "Hub88 Sweepstakes Coin") },
+            { "SC.", new CurrencyData("SC.", "SC {0}", desc: "Sweepstakes Coin via Relax P2P") },
+            { "XSC", new CurrencyData("XSC", "SC {0}", desc: "Generic Sweepstakes Coin") },
+            { "GCC", new CurrencyData("GCC", "GC {0}", desc: "Markor Gold Coin") },
+            { "XGC", new CurrencyData("XGC", "GC {0}", desc: "Generic Gold Coin") },
+            { "GC.", new CurrencyData("GC.", "GC {0}", desc: "Gold Coin via Relax P2P") },
+            { "GLD", new CurrencyData("GLD", "GC {0}", desc: "Hub88 Gold Coin") },
+            { "SFC", new CurrencyData("SFC", "FC {0}", desc: "Markor Sweepstakes Coin (Casimba)") },
+            { "FCC", new CurrencyData("FCC", "FC {0}", desc: "(Fortune) Sweepstakes Coin") },
+            { "FC.", new CurrencyData("FC.", "FC {0}", desc: "Relax Fortune Coin") },
+            { "BT.", new CurrencyData("BT.", "BT {0}", desc: "Relax Bit") },
+            { "BK.", new CurrencyData("BK.", "BK {0}", desc: "Relax Buck") },
+            { "VBC", new CurrencyData("VBC", "WOW {0}", desc: "Markor VBC Fun Coin") },
         };
 
+        private static readonly Dictionary<string, CurrencyData> _cryptoCurrencies = new Dictionary<string, CurrencyData>
+        {
+            { "MBTC", new CurrencyData("MBTC", "mBTC {0}", 3, 0.00001d, "Milli Bitcoin") },
+            { "BTC", new CurrencyData("BTC", "BTC {0}", 5, 0.00000001d, "Bitcoin") },
+        };
+        
         public static void SetSession(FireballSession session)
         {
             _session = session;
@@ -65,10 +71,16 @@ namespace Fireball.Game.Client.Tools
         {
             return _virtualCurrencies.Keys.Contains(currencyCode.ToUpper());
         }
+        
+        public static bool IsCryptoCurrency(string currencyCode)
+        {
+            return _cryptoCurrencies.Keys.Contains(currencyCode.ToUpper());
+        }
 
         public static string FormatMoney(long money, string customCurrency = null, CultureInfo customCulture = null)
         {
-            var currency = customCurrency ?? GetCurrentSession()?.Currency;
+            var session = GetCurrentSession();
+            var currency = customCurrency ?? session?.Currency;
             var culture = customCulture ?? GetCurrentCulture();
             
             // format fiat currencies
@@ -82,12 +94,22 @@ namespace Fireball.Game.Client.Tools
             {
                 if (data != null)
                 {
-                    return string.Format(data.SymbolFormat, (money * data.DecimalsMultiplier).ToString($"N{data.Decimals}", culture));
+                    var virtualMultiplier = session?.Multiplier != null ? 1.0d / session.Multiplier.Value : data.DecimalsMultiplier;
+                    return string.Format(data.SymbolFormat, (money * virtualMultiplier).ToString($"N{data.Decimals}", culture));
                 }
                 return (money * 0.01d).ToString("N2", culture);
             }
             
-            var session = GetCurrentSession();
+            // format crypto currencies
+            if (currency != null && _cryptoCurrencies.TryGetValue(currency.ToUpper(), out data))
+            {
+                if (data != null)
+                {
+                    var cryptoMultiplier = session?.Multiplier != null ? 1.0d / session.Multiplier.Value : data.DecimalsMultiplier;
+                    return string.Format(data.SymbolFormat, (money * cryptoMultiplier).ToString($"N{data.Decimals}", culture));
+                }
+            }
+            
             var decimalsMultiplier = session?.Multiplier != null ? 1.0d / session.Multiplier.Value : 0.01d;
             var decimals = session?.Multiplier != null ? CalculateDecimals(session.Multiplier.Value) : 2;
             return string.Format(currency + " {0}", (money * decimalsMultiplier).ToString($"N{decimals}", culture));
